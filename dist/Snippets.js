08cf7453eb3b36a64003f557f944b21f7d663629
khacminh <khacminhrt@gmail.com>
2023-06-07 09:39:32 +0700

    update serverside query - optional

--- dist/Snippets.js
+++ dist/Snippets.js
@@ -20,17 +20,32 @@ var Snippets = {
         jsDir = _ref.jsDir,
         htmlDir = _ref.htmlDir;
 
-    var gtm_auth = '&gtm_auth=' + auth;
-    var gtm_preview = '&gtm_preview=' + preview;
+    var gtm_auth = auth ? '&gtm_auth=' + auth : '';
+    var gtm_preview = preview ? '&gtm_preview=' + preview : '';
+    var gtm_cookies_win = auth || preview ? '&gtm_cookies_win=x' : '';
+    var serverSideQuery = '' + gtm_auth + gtm_preview + gtm_cookies_win;
+    var serverSideQueryForScript = serverSideQuery ? '+' + serverSideQuery : '';
+
     var gtm_src = src;
     var iframeSrc = htmlDir ? gtm_src + '/' + htmlDir : gtm_src;
     var jsSrc = jsDir ? gtm_src + '/' + jsDir : gtm_src;
 
     if (!id) (0, _warn2.default)('GTM Id is required');
 
-    var iframe = '\n      <iframe src="' + iframeSrc + '/ns.html?id=' + id + gtm_auth + gtm_preview + '&gtm_cookies_win=x"\n        height="0" width="0" style="display:none;visibility:hidden" id="tag-manager"></iframe>';
+    // const iframe = `
+    //   <iframe src="${iframeSrc}/ns.html?id=${id}${gtm_auth}${gtm_preview}&gtm_cookies_win=x"
+    //     height="0" width="0" style="display:none;visibility:hidden" id="tag-manager"></iframe>`
+    // const script = `
+    //   (function(w,d,s,l,i){w[l]=w[l]||[];
+    //     w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${JSON.stringify(events).slice(1, -1)}});
+    //     var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+    //     j.async=true;j.src='${jsSrc}/gtm.js?id='+i+dl'${serverSideQueryForScript}';
+    //     f.parentNode.insertBefore(j,f);
+    //   })(window,document,'script','${dataLayerName}','${id}');`
 
-    var script = '\n      (function(w,d,s,l,i){w[l]=w[l]||[];\n        w[l].push({\'gtm.start\': new Date().getTime(),event:\'gtm.js\', ' + JSON.stringify(events).slice(1, -1) + '});\n        var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';\n        j.async=true;j.src=\'' + jsSrc + '/gtm.js?id=\'+i+dl+\'' + gtm_auth + gtm_preview + '&gtm_cookies_win=x\';\n        f.parentNode.insertBefore(j,f);\n      })(window,document,\'script\',\'' + dataLayerName + '\',\'' + id + '\');';
+    var iframe = '\n      <iframe src="' + iframeSrc + '/ns.html?id=' + id + serverSideQuery + '"\n        height="0" width="0" style="display:none;visibility:hidden"></iframe>';
+
+    var script = '\n      (function(w,d,s,l,i){w[l]=w[l]||[];\n        w[l].push({\'gtm.start\': new Date().getTime(),event:\'gtm.js\', ' + JSON.stringify(events).slice(1, -1) + '});\n        var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';\n        j.async=true;j.src=\'' + jsSrc + '/gtm.js?id=\'+i+dl\'' + serverSideQueryForScript + '\';\n        f.parentNode.insertBefore(j,f);\n      })(window,document,\'script\',\'' + dataLayerName + '\',\'' + id + '\');';
 
     var dataLayerVar = this.dataLayer(dataLayer, dataLayerName);
 
--- src/Snippets.js
+++ src/Snippets.js
@@ -4,23 +4,38 @@ import warn from './utils/warn'
 
 const Snippets = {
   tags: function ({ id, src, events, dataLayer, dataLayerName, preview, auth, jsDir, htmlDir }) {
-    const gtm_auth = `&gtm_auth=${auth}`
-    const gtm_preview = `&gtm_preview=${preview}`
+    const gtm_auth = auth ? `&gtm_auth=${auth}` : '';
+    const gtm_preview = preview ? `&gtm_preview=${preview}` : '';
+    const gtm_cookies_win = auth || preview ? '&gtm_cookies_win=x' : '';
+    const serverSideQuery = `${gtm_auth}${gtm_preview}${gtm_cookies_win}`;
+    const serverSideQueryForScript = serverSideQuery ? `+${serverSideQuery}` : '';
+
     const gtm_src = src
     const iframeSrc = htmlDir ? `${gtm_src}/${htmlDir}` : gtm_src;
     const jsSrc = jsDir ? `${gtm_src}/${jsDir}` : gtm_src;
 
     if (!id) warn('GTM Id is required')
 
+    // const iframe = `
+    //   <iframe src="${iframeSrc}/ns.html?id=${id}${gtm_auth}${gtm_preview}&gtm_cookies_win=x"
+    //     height="0" width="0" style="display:none;visibility:hidden" id="tag-manager"></iframe>`
+    // const script = `
+    //   (function(w,d,s,l,i){w[l]=w[l]||[];
+    //     w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${JSON.stringify(events).slice(1, -1)}});
+    //     var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+    //     j.async=true;j.src='${jsSrc}/gtm.js?id='+i+dl'${serverSideQueryForScript}';
+    //     f.parentNode.insertBefore(j,f);
+    //   })(window,document,'script','${dataLayerName}','${id}');`
+
     const iframe = `
-      <iframe src="${iframeSrc}/ns.html?id=${id}${gtm_auth}${gtm_preview}&gtm_cookies_win=x"
-        height="0" width="0" style="display:none;visibility:hidden" id="tag-manager"></iframe>`
+      <iframe src="${iframeSrc}/ns.html?id=${id}${serverSideQuery}"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`
 
     const script = `
       (function(w,d,s,l,i){w[l]=w[l]||[];
         w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${JSON.stringify(events).slice(1, -1)}});
         var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-        j.async=true;j.src='${jsSrc}/gtm.js?id='+i+dl+'${gtm_auth}${gtm_preview}&gtm_cookies_win=x';
+        j.async=true;j.src='${jsSrc}/gtm.js?id='+i+dl'${serverSideQueryForScript}';
         f.parentNode.insertBefore(j,f);
       })(window,document,'script','${dataLayerName}','${id}');`
 
